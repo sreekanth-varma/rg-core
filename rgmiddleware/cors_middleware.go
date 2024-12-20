5acbd12b1d3f81f8b9120e321ecb2b47550cf927
sreekanthvarma.ayyapuraju <sreekanthvarma.ayyapuraju@tectoro.com>
2024-12-20 11:02:38 +0530

    changes

--- rgmiddleware/cors_middleware.go
+++ rgmiddleware/cors_middleware.go
@@ -12,6 +12,7 @@ func CORSMiddleware() gin.HandlerFunc {
 	log.Println("cors enabled")
 	return func(c *gin.Context) {
 		if isEnabled != "Y" {
+			log.Println("Cors is in disabled")
 			c.Next()
 			return
 		}
@@ -20,12 +21,12 @@ func CORSMiddleware() gin.HandlerFunc {
 		c.Writer.Header().Set("Access-Control-Expose-Headers", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, X-Encrypted")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "OPTIONS,GET,POST,PUT,PATCH,DELETE")
+		log.Println("cors .................")
 
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
 			return
 		}
-
 		c.Next()
 	}
 }
